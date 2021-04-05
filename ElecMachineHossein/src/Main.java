68b43d2e59b67c591939a8b918eeeaf6ee308855
Hosseinhgz <73482113+Hosseinhgz@users.noreply.github.com>
2021-04-05 07:43:31 +0300

    create comparison
    some calculations not working properly

--- ElecMachineHossein/src/Main.java
+++ ElecMachineHossein/src/Main.java
@@ -5,6 +5,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Random;
+import java.lang.Math;
+
 
 /**
  *
@@ -26,8 +29,20 @@ public class Main {
         ResultSet rs = null;
 
         
-        // IN GENERAl try and catches are here to help us find our errors and where they happen.
+        Random rand = new Random();
         
+        int num = 4; //number of candidates
+        int que = 19; // number of question
+        
+        int[] customer1_Ans= new int[que];
+        for (int k=0; k < customer1_Ans.length;k++) {
+            int n = 1 + rand.nextInt(5);
+        	customer1_Ans[k]= n;
+        	System.out.println("customer1 question"+(k+1)+ " answer is "+ customer1_Ans[k]);
+        }
+        
+        
+        // IN GENERAl try and catches are here to help us find our errors and where they happen.   
         
         try {
             Class.forName(driver).newInstance();
@@ -45,18 +60,22 @@ public class Main {
 
             stmt = con.createStatement();
             String sql;
+            
+	        // 1st part is candidate ID and 2nd part is question number
+	        int[][] ans = new int[num][que];
 
             // Here we are going through all answers from candidate which is available in database in answers TABLE
             // we have also have an array for this customer answers
             // We compare the answers of a customer for all question (1 by 1 for answers)
             
-            // going through Candidates (for example for first candidate)
-            for(int i = 1; i < 2; i++){
-            	System.out.println(i);
-            	// going through questions (19 question available)
+            // going through Candidates (for example for 4 first candidate)
+            for(int i = 1; i <= num; i++){
+            	
+            	// for test i value
+            	//System.out.println(i);
+            	
+            	// going through questions (que=19 question available)
                 	
-        	        // 1st part is candidate ID and 2nd part is question number
-        	        int[][] ans = new int[1][19];
         	        
                 	sql = "SELECT * FROM ANSWERS";
                 	
@@ -76,13 +95,33 @@ public class Main {
             	            ans[i-1][j-1] = Answer;
             	            
             	            // for test that ans array is working
-            	            System.out.println(ans[i-1][j-1]);
+            	            System.out.println("candidate "+i+" question "+j+ " is "+ans[i-1][j-1]);
             	            // System.out.println("Answer of candidate "+i+" for question "+j+ " is "+ ans[i-1][j-1]);
             	            j++;
         	        	}
         	        }
   	
-                }  
+                }
+            // here we are going to calculate the point of customer1 similarity to each candidate in percent
+            double[][] point = new double[1][num];
+            for (int m=0;m<num;m++) {
+                point[0][m] = 0;
+            	for(int n=0;n<que;n++) {
+                    System.out.println("Ready?!? "+ m + " "+ n);
+                    
+                    //for test
+                    System.out.println("candidateAns "+ans[m][n]);
+                    System.out.println("customer "+ customer1_Ans[n]);
+                    double abs = 0;
+                    if(ans[m][n]<customer1_Ans[n]) {
+                    	abs = (4-customer1_Ans[n]+ans[m][n])/4;
+                    }else {
+                    	abs =(4-ans[m][n]+customer1_Ans[n])/4;
+                    }
+                    point[0][m] = point[0][m]+abs;
+                    System.out.println(point[0][m]);
+            	}
+            }
             	
             
     
